67985e887025b58606edf24390f6c389d6dea407
unknown <othmaneelahmadisweb@gmail.com>
2022-03-14 18:10:45 +0100

    [feat]/CALC-2

--- git-demostration/src/main/java/org/git_demostration/App.java
+++ git-demostration/src/main/java/org/git_demostration/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(Calculator.add(10, 20));
+        System.out.println(Calculator.substruction(10, 20));
+        System.out.println(Calculator.multiplying(10, 20));
+        System.out.println(Calculator.division(10, 0));
     }
 }
